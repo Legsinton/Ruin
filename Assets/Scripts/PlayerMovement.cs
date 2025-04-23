b7455e2b7dc504b29a16f77a0419a9dfe5899f22
Legsinton <alexander_raberg@hotmail.com>
2025-04-23 16:37:27 +0200

    added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
+
     [Header("Movement")]
 
     public float currentVelocity;
 
-    public float currentSpeed;
+    private float movementX;
+    private float movementZ;
 
+    public float currentSpeed;
+    
     public float acceleration;
 
     public float groundDrag;
 
-    public InputAction action;
+    public float pullSpeed;
 
-
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
-
-    Vector3 moveDirection;
+    float pullDistance = 1.5f;
 
     Rigidbody rb;
+
+    private bool isInteracting = false;
+
+    bool isPulling = false;
+
+    GameObject targetBlock;
 
     private void Start()
     {
@@ -34,53 +40,84 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
-    private void OnEnable()
+    private void OnMove(InputValue movementValue)
     {
-        action.Enable();
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementZ = movementVector.y;
     }
 
-    private void OnDisable()
+    private void OnInteract(InputValue value)
     {
-        action.Disable();
-    }
-
-    private void Update()
-    {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-
-        if (grounded)
+        if (value.isPressed)
         {
-            rb.linearDamping = groundDrag;
+            isInteracting = true;
+            Debug.Log("Interact Pressed");
         }
         else
         {
-            rb.linearDamping = 0;
+            rb.linearDamping = 1;
+            rb.mass = 1;
+            currentSpeed = 10;
+            isPulling = false;
+            isInteracting = false;
+            Debug.Log("Interact Released");
         }
     }
 
     private void FixedUpdate()
     {
         MovePlayer();
+
+        IsPulling();    
+    }
+
+    private void IsPulling()
+    {
+        if (isInteracting && targetBlock != null && isPulling)
+        {
+            // Direction player is moving in
+            Vector3 moveDir = (transform.right * movementX + transform.forward * movementZ).normalized;
+            //rb.mass = 5;
+            //rb.linearDamping = 5;
+            currentSpeed = 4;
+            // If the player is moving, pull the block
+            if (moveDir.magnitude > 0f)
+            {
+                targetBlock.transform.position += moveDir * pullSpeed * Time.deltaTime;
+            }
+        }
     }
 
     private void MovePlayer()
     {
-        moveDirection = action.ReadValue<Vector3>();
+        Vector3 movement = new Vector3(movementZ, 0.0f, -movementX);
 
-        if (moveDirection.magnitude > 0)
+        if (movement.magnitude > 0)
         {
             currentVelocity = Mathf.MoveTowards(currentVelocity, currentSpeed, acceleration * Time.deltaTime);
-
         }
 
         else
         {
-            currentVelocity = Mathf.MoveTowards(currentVelocity, 0, groundDrag * Time.deltaTime);
+            currentVelocity = Mathf.MoveTowards(currentVelocity, 4, groundDrag * Time.deltaTime);
         }
 
-            rb.transform.Translate(moveDirection.normalized * currentVelocity);
-
-        
-      
+        //rb.transform.Translate(movement.normalized * currentVelocity);
+        rb.linearVelocity = movement.normalized * currentVelocity;
     }
+
+    private void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Pullable")) // Optional: filter by tag
+        {
+            Debug.Log("It Collides");
+            targetBlock = hit.gameObject;
+            isPulling = true;
+        }
+
+
+    }
+
 }
