50c723c0c1deb18d2483a72af03ca2672b4e51d6
Legsinton <alexander_raberg@hotmail.com>
2025-04-28 15:48:16 +0200

    new UI

--- Assets/Scripts/PaintingScript.cs
+++ Assets/Scripts/PaintingScript.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 public class PaintingScript : MonoBehaviour, IInteracting
 {
     [SerializeField] Outline outlineScript;
+    [SerializeField] TextMeshProUGUI paintingText;
+    [SerializeField] Image background;
+
     [SerializeField] UIScript script;
 
     private void Awake()
     {
         script = FindAnyObjectByType<UIScript>();
+        paintingText.enabled = false;
+        background.enabled = false;
     }
 
     public void PressInteract()
     {
-        
+        paintingText.enabled = !paintingText.enabled;
+        background.enabled = !background.enabled;
 
         Debug.Log("Give Me Money");
     }
@@ -31,5 +37,7 @@ public class PaintingScript : MonoBehaviour, IInteracting
     {
         script.DisebleUI();
         outlineScript.enabled = false;
+        paintingText.enabled = false;
+        background.enabled = false;
     }
 }
