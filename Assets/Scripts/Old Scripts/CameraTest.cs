fcb1aba89811d3ff7a6accfa0731cda0260e1d38
Mojkan <elvinhillstrom@gmail.com>
2025-05-11 15:08:20 +0200

    Updated camera test script

--- Assets/Scripts/Old Scripts/CameraTest.cs
+++ Assets/Scripts/Old Scripts/CameraTest.cs
@@ -4,25 +4,20 @@ using UnityEngine.InputSystem;
 public class CameraTest : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] Vector3 cameraOffset;
+    [SerializeField] Vector3 cameraPlayerOffset;
     [SerializeField] Vector3 cameraFocusOffset;
     [SerializeField] float smoothTime;
     [SerializeField] float rotationSpeed;
-    [SerializeField] float cameraDistance;
-    [SerializeField] float minCameraDistance;
+    [SerializeField] Vector2 rotationClamp;
+    [SerializeField] float targetCameraDistance;
     [SerializeField] float wallDistance;
 
     [Header("Reference")]
     [SerializeField] Transform cameraTransform;
 
+    Vector3 focusTarget;
     Vector2 mouseDelta;
     Vector2 currentRotation;
-
-    bool disableRotateRight = false;
-    bool disableRotateLeft = false;
-
-    float distanceToplayer;
-    bool movingCamera;
 
     void Start()
     {
@@ -43,100 +38,41 @@ public class CameraTest : MonoBehaviour
 
     void UpdateCameraPosition()
     {
-        // Save last camera position and last mmouse x value
-        Vector3 lastCameraPos = cameraTransform.position;
-        Vector2 lastRotation = currentRotation;
-
-       // mouseDelta = mouseDelta;
-
         currentRotation += mouseDelta * rotationSpeed;
 
-        currentRotation.y = Mathf.Clamp(currentRotation.y, -20, 60);
-
-        // Check if left or right horizontal rotation is disabled
-        if (disableRotateRight)
-        {
-            if (currentRotation.x < lastRotation.x)
-            {
-                currentRotation.x = lastRotation.x;
-            }
-            else
-            {
-                disableRotateRight = false;
-            }
-        }
-        if (disableRotateLeft)
-        {
-            if (currentRotation.x > lastRotation.x)
-            {
-                currentRotation.x = lastRotation.x;
-            }
-            else
-            {
-                disableRotateLeft = false;
-            }
-        }
-
+        currentRotation.y = Mathf.Clamp(currentRotation.y, rotationClamp.x, rotationClamp.y);
 
         // Calculated the next camera position
         Quaternion rotation = Quaternion.Euler(-currentRotation.y, currentRotation.x, 0);
 
-        Vector3 newCameraOffset = rotation * cameraOffset * cameraDistance;
+        Vector3 newCameraOffset = rotation * cameraPlayerOffset.normalized * targetCameraDistance;
 
         Vector3 newCameraPos = transform.position + newCameraOffset;
 
-        Vector3 direction = newCameraPos - lastCameraPos;
-
-        // Check if the new position will collide with the camera
-        float distance = direction.magnitude;
-        if (!movingCamera && Physics.Raycast(lastCameraPos, direction.normalized, out RaycastHit hit2, distance + wallDistance, LayerMask.GetMask("Wall")))
+        // Check wall collision
+        Vector3 direction = transform.position - newCameraPos;
+        float distance = Vector3.Distance(transform.position, newCameraPos);
+        if (Physics.Raycast(transform.position, -direction.normalized, out RaycastHit hit1, distance + wallDistance, LayerMask.GetMask("Wall")))
         {
-            newCameraPos = hit2.point + hit2.normal * wallDistance;
-
-            Vector3 hitDirection = hit2.point - lastCameraPos;
-            float dotProduct = Vector3.Dot(hitDirection, cameraTransform.right);
-            float dotProductUp = Vector3.Dot(hitDirection, cameraTransform.up);
-
-            if (dotProduct > 0)
-            {
-                //If the raycast hit the right of the camera
-                disableRotateRight = true;
-            }
-            else
-            {
-                //If the raycast hit the left of the camera
-                disableRotateLeft = true;
-            }
+            newCameraPos = new Vector3(hit1.point.x, newCameraPos.y, hit1.point.z) + direction.normalized * wallDistance;
         }
 
-        distanceToplayer = Vector3.Distance(cameraTransform.TransformPoint(cameraFocusOffset), transform.position);
-        if (!movingCamera && distanceToplayer < minCameraDistance)
-        {
-            currentRotation.x -= 180;
+        // Update camera position
+        //cameraTransform.position = Vector3.Slerp(cameraTransform.position, newCameraPos, smoothTime * Time.deltaTime);
+        cameraTransform.position = newCameraPos;
 
-            rotation = Quaternion.Euler(-currentRotation.y, currentRotation.x, 0);
+        // Update focus point position
+        Vector3 newFocusPos = transform.position - newCameraOffset + cameraFocusOffset;
 
-            newCameraOffset = rotation * cameraOffset * cameraDistance;
-
-            newCameraPos = transform.position + newCameraOffset;
-
-            movingCamera = true;
-        }
-        if (distanceToplayer - 0.5 > minCameraDistance)
-        {
-            movingCamera = false;
-        }
-
-        cameraTransform.position = Vector3.Slerp(cameraTransform.position, newCameraPos, smoothTime * Time.deltaTime);   
+        //focusTarget = Vector3.Slerp(focusTarget, newFocusPos, smoothTime * Time.deltaTime);
+        focusTarget = newFocusPos;
     }
 
     void UpdateCameraRotation()
     {
-        Vector3 directionToPlayer = transform.position - cameraTransform.TransformPoint(cameraFocusOffset);
+        Vector3 directionToFocus = focusTarget - cameraTransform.position;
 
-        Quaternion angleToPlayer = Quaternion.LookRotation(directionToPlayer);
-
-        Quaternion targetRotation = Quaternion.Euler(angleToPlayer.eulerAngles.x, angleToPlayer.eulerAngles.y, 0);
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFocus.normalized);
 
         cameraTransform.rotation = targetRotation;
     }
