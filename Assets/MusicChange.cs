1105aab11f1058df8af8b255da6e23e4a32bd6e0
Legsinton <alexander_raberg@hotmail.com>
2025-05-12 16:57:27 +0200

    Update MusicChange.cs

--- Assets/MusicChange.cs
+++ Assets/MusicChange.cs
@@ -4,6 +4,12 @@ public class MusicChange : MonoBehaviour
 {
     bool played;
     [SerializeField] string music;
+
+    private void Start()
+    {
+        MusicManager.Instance.PlayMusic("Scary");
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
