14f2681b707d7b737c1133500bad33a465de365d
Legsinton <alexander_raberg@hotmail.com>
2025-05-16 18:01:24 +0200

    FIxed and updated the soundfxManager

--- Assets/Scripts/Sound Scripts/SoundFXManager.cs
+++ Assets/Scripts/Sound Scripts/SoundFXManager.cs
@@ -1,20 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
-using static Unity.VisualScripting.Member;
 
 public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager Instance;
 
-    [SerializeField] private AudioSource soundFXObject;
-    [SerializeField] private AudioSource[] loopFXSources;  // Array to hold multiple looping audio sources
-    private Dictionary<SoundType, object> soundFXDict;
-    [SerializeField] private List<SoundVolumeEntry> soundVolumeList;
-   // private Dictionary<SoundType, float> soundVolumeDict;
- 
+    [SerializeField] 
+    AudioSource soundFXObject;
+    [SerializeField]
+     List<SoundVolumeEntry> volumeList;
+
+    Dictionary<SoundType, object> soundFXDict;
+
+    Dictionary<SoundType, float> soundVolumeDict = new Dictionary<SoundType, float>();
+
+    Dictionary<GameObject, LoopSoundInstance> activeLoopsByObject = new Dictionary<GameObject, LoopSoundInstance>();
+
     public class LoopSoundInstance
     {
         public AudioSource source;
@@ -25,13 +26,13 @@ public class SoundFXManager : MonoBehaviour
         public float targetVolume;
     }
 
-    private Dictionary<GameObject, LoopSoundInstance> activeLoopsByObject = new Dictionary<GameObject, LoopSoundInstance>();
-
-    public void StopLoop(LoopSoundInstance instance)
+    [System.Serializable]
+    public class SoundVolumeEntry
     {
-        if (instance != null)
-            instance.stop = true;
+        public SoundType soundType;  // The sound effect type (key)
+        [Range(0, 1)] public float volume;         // The volume level (value)
     }
+
     private void Awake()
     {
         if (Instance == null)
@@ -46,54 +47,6 @@ public class SoundFXManager : MonoBehaviour
 
         InitializeSounds();
     }
-
-    [System.Serializable]
-    public class SoundVolumeEntry
-    {
-        public SoundType soundType;  // The sound effect type (key)
-        [Range(0,1)]public float volume;         // The volume level (value)
-    }
-
-    [SerializeField]
-    private List<SoundVolumeEntry> volumeList;
-
-    private Dictionary<SoundType, float> soundVolumeDict = new Dictionary<SoundType, float>();
-
-    private void OnValidate()
-    {
-        if (soundVolumeList != null)
-        {
-            soundVolumeDict = new Dictionary<SoundType, float>();
-            foreach (var entry in soundVolumeList)
-            {
-                soundVolumeDict[entry.soundType] = entry.volume;
-            }
-        }
-    }
-
-    private void InitializeSounds()
-    {
-        soundFXDict = new Dictionary<SoundType, object>
-            {
-                //Single AudioClips
-                { SoundType.Chain, Resources.Load<AudioClip>("Sounds/Effects/Chain") },
-                { SoundType.ChestCreak, Resources.Load<AudioClip>("Sounds/Effects/ChestCreak") },
-                { SoundType.ChestOpen, Resources.Load<AudioClip>("Sounds/Effects/ChestOpen") },
-                { SoundType.Death, Resources.Load<AudioClip>("Sounds/Effects/Death") },
-                { SoundType.Fire, Resources.Load<AudioClip>("Sounds/Effects/Fire") },
-                { SoundType.KeyFound, Resources.Load<AudioClip>("Sounds/Effects/KeyFound") },
-                { SoundType.PuzzleSolved, Resources.Load<AudioClip>("Sounds/Effects/PuzzleSolved") },
-                { SoundType.PuzzleSolvedFully, Resources.Load<AudioClip>("Sounds/Effects/PuzzleSolvedFully") },
-                { SoundType.Roll, Resources.Load<AudioClip>("Sounds/Effects/Roll") },
-                { SoundType.PushBlock, Resources.Load<AudioClip>("Sounds/Effects/PushBlock") },
-
-                //AudioClips
-                { SoundType.Launch, Resources.LoadAll<AudioClip>("Sounds/Effects/Launch") },
-                { SoundType.RandomScary,Resources.LoadAll<AudioClip>("Sounds/Effects/RandomScary") },
-                { SoundType.Walk, Resources.LoadAll<AudioClip>("Sounds/Effects/Walk") },
-            };
-    }
-
     private void Update()
     {
         // Update fading for each active loop
@@ -135,42 +88,40 @@ public class SoundFXManager : MonoBehaviour
 
     }
 
-   /* public void PlaySoundFX( SoundType type, Vector3? position = null)
+    private void OnValidate()
     {
-        if (!soundFXDict.ContainsKey(type)) return;
+        if (volumeList != null)
+        {
+            soundVolumeDict = new Dictionary<SoundType, float>();
+            foreach (var entry in volumeList)
+            {
+                soundVolumeDict[entry.soundType] = entry.volume;
+            }
+        }
+    }
 
-        float volume = 1.0f;
-        if (soundVolumeDict != null && soundVolumeDict.ContainsKey(type))
-        {
-            volume = soundVolumeDict[type];
-        }
-        if (soundFXDict[type] is AudioClip singleClip)
-        {
-            if (position.HasValue)
+    private void InitializeSounds()
+    {
+        soundFXDict = new Dictionary<SoundType, object>
             {
-                AudioSource.PlayClipAtPoint(singleClip, position.Value);
-            }
-            else
-            {
-                Debug.Log("Its fucked");
-                soundFXObject.PlayOneShot(singleClip, volume);
-            }
-        }
-        else if (soundFXDict[type] is AudioClip[] clipArray)
-        {
-            int rand = Random.Range(0, clipArray.Length);
-            var selectedClip = clipArray[rand];
+                //Single AudioClips
+                { SoundType.Chain, Resources.Load<AudioClip>("Sounds/Effects/Chain") },
+                { SoundType.ChestCreak, Resources.Load<AudioClip>("Sounds/Effects/ChestCreak") },
+                { SoundType.ChestOpen, Resources.Load<AudioClip>("Sounds/Effects/ChestOpen") },
+                { SoundType.Death, Resources.Load<AudioClip>("Sounds/Effects/Death") },
+                { SoundType.Fire, Resources.Load<AudioClip>("Sounds/Effects/Fire") },
+                { SoundType.KeyFound, Resources.Load<AudioClip>("Sounds/Effects/KeyFound") },
+                { SoundType.PuzzleSolved, Resources.Load<AudioClip>("Sounds/Effects/PuzzleSolved") },
+                { SoundType.PuzzleSolvedFully, Resources.Load<AudioClip>("Sounds/Effects/PuzzleSolvedFully") },
+                { SoundType.Roll, Resources.Load<AudioClip>("Sounds/Effects/Roll") },
+                { SoundType.PushBlock, Resources.Load<AudioClip>("Sounds/Effects/PushBlock") },
 
-            if (position.HasValue)
-            {
-                AudioSource.PlayClipAtPoint(selectedClip, position.Value, volume);
-            }
-            else
-            {
-                soundFXObject.PlayOneShot(selectedClip, volume);
-            }
-        }
-    }*/
+                //AudioClips
+                { SoundType.RandomScary,Resources.LoadAll<AudioClip>("Sounds/Effects/RandomScary") },
+                { SoundType.Walk, Resources.LoadAll<AudioClip>("Sounds/Effects/Walk") },
+            };
+    }
+
     public void PlaySoundFX(SoundType type, Vector3? position = null, float minDistance = 1f, float maxDistance = 50f, float spatialBlend = 1f)
     {
         if (!soundFXDict.ContainsKey(type)) return;
@@ -215,7 +166,6 @@ public class SoundFXManager : MonoBehaviour
         }
     }
 
-
     public void StartLoopFor(GameObject owner, SoundType type,Transform parent = null)
     {
         if (activeLoopsByObject.ContainsKey(owner)) return; // If there's already a loop for this object, don't start another
@@ -246,15 +196,6 @@ public class SoundFXManager : MonoBehaviour
             Debug.LogError($"[SoundFXManager] AudioClip for {type} is null.");
             return;
         }
-        // Load clip
-        /*AudioClip clip = Resources.Load<AudioClip>($"Sounds/Effects/{type}");
-        if (clip == null)
-        {
-            Debug.LogError($"[SoundFXManager] AudioClip not found for: {type}");
-            return;
-        }*/
-
-        // Create new GameObject for the sound
         GameObject tempGO = new GameObject($"3D_Loop_{owner.name}_{type}");
         if (parent != null)
         {
@@ -298,19 +239,17 @@ public enum SoundType
 {
     // Array Of Sounds
     Break,
-    Death,
-    Launch,
     Chain,
-    Walk,
-    Roll,
-    PushBlock,
-    RandomScary,
     ChestCreak,
+    ChestOpen,
+    Death,
+    Fire,
+    KeyFound,
+    PushBlock,
     PuzzleSolved,
     PuzzleSolvedFully,
-    ChestOpen,
-    KeyFound,
-    Fire
-
+    Roll,
+    RandomScary,
+    Walk
 }
 
