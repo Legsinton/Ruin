a481823798bb9c6cff355b78112c439acf85a24f
Mojkan <elvinhillstrom@gmail.com>
2025-05-19 16:49:49 +0200

    Changed triggerblock and rotatingobject

--- Assets/Scripts/Puzzle Objects/RotatingObject.cs
+++ Assets/Scripts/Puzzle Objects/RotatingObject.cs
@@ -10,6 +10,9 @@ public class RotatingObject : MonoBehaviour, IInteracting
     [SerializeField] float interactRange;
     [SerializeField] Vector2 clampRotation;
 
+    [Header("Clock Puzzel Settings")]
+    [SerializeField] bool clockPuzzle;
+
     [Header("Reference")]
     [SerializeField] Transform playerTransform;
     [SerializeField] Transform centerPoint;
@@ -23,7 +26,7 @@ public class RotatingObject : MonoBehaviour, IInteracting
     bool playerAttached;
     bool calculatedPlayerPos;
 
-    [HideInInspector] public event Action<float> UpdateTriggerBlocks;
+    [HideInInspector] public event Action<RotatingObject, float> UpdateTriggerBlocks;
 
     void Awake()
     {
@@ -69,6 +72,7 @@ public class RotatingObject : MonoBehaviour, IInteracting
                 {
                     if (Vector3.Distance(playerTransform.position, targetPos) > 0.1)
                     {
+                        targetPos.y = playerTransform.position.y;
                         playerTransform.position = Vector3.Lerp(playerTransform.position, targetPos, correctPlayerPosSpeed * Time.deltaTime);
                     }
                     else
@@ -82,17 +86,20 @@ public class RotatingObject : MonoBehaviour, IInteracting
                 float input = playerMovement.movementInput.y;
                 float angle = -input * rotateSpeed * Time.deltaTime;
 
-                if (centerPoint.rotation.eulerAngles.y + angle > clampRotation.y)
+                if (!clockPuzzle)
                 {
-                    angle = clampRotation.y - centerPoint.rotation.eulerAngles.y;
-                }
-                else if (centerPoint.rotation.eulerAngles.y + angle < clampRotation.x)
-                {
-                    angle = clampRotation.x - centerPoint.rotation.eulerAngles.y;
+                    if (centerPoint.rotation.eulerAngles.y + angle > clampRotation.y)
+                    {
+                        angle = clampRotation.y - centerPoint.rotation.eulerAngles.y;
+                    }
+                    else if (centerPoint.rotation.eulerAngles.y + angle < clampRotation.x)
+                    {
+                        angle = clampRotation.x - centerPoint.rotation.eulerAngles.y;
+                    }
                 }
 
                 centerPoint.Rotate(Vector3.up, angle);
-                UpdateTriggerBlocks?.Invoke(centerPoint.rotation.eulerAngles.y);
+                UpdateTriggerBlocks?.Invoke(this, centerPoint.rotation.eulerAngles.y);
             }
         }
         else if (playerAttached)
