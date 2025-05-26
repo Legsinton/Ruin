72e44b5bdfa63b581c674b897029ed0a6aef7ee2
Agnes Binett <agnesbinett@gmail.com>
2025-05-26 17:20:27 +0200

    Rotate object in inspector

--- Assets/Scripts/Inspector/InspectableItem.cs
+++ Assets/Scripts/Inspector/InspectableItem.cs
@@ -31,7 +31,7 @@ public class InspectableItem : MonoBehaviour, IInteracting
         if (canInteract)
         {
             Debug.Log("Agnes: Can interact");
-            InputHandler.Instance.OnEnable();
+            //InputHandler.Instance.OnEnable();
         }
         inspector.InspectItem(item);
 
--- Assets/Scripts/Inspector/Inspector.cs
+++ Assets/Scripts/Inspector/Inspector.cs
@@ -1,13 +1,19 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Inspector : MonoBehaviour
 {
     Canvas canvas;
+    //InputSystem_Actions inspectInput;
+    PlayerInput playerInput;
+    GameObject itemPrefab;
+    bool isRotating = false;
+
 
     void Awake()
     {
         GameObject inspectorCanvas = GameObject.Find("InspectorCanvas");
+        var actions = GetComponent<PlayerInput>().actions;
         if (inspectorCanvas != null)
         {
             canvas = inspectorCanvas.GetComponent<Canvas>();
@@ -16,6 +22,14 @@ public class Inspector : MonoBehaviour
         else
         {
             Debug.Log("Could not locate Canvas component on " + inspectorCanvas.name);
+        }
+
+        //inspectInput = new InputSystem_Actions();
+        playerInput = FindFirstObjectByType<PlayerInput>();
+
+        if (playerInput == null)
+        {
+            Debug.LogError("Player Input Component is null");
         }
     }
 
@@ -26,13 +40,19 @@ public class Inspector : MonoBehaviour
         Item item = (Item)interactableObject;
         if (item != null && item.prefab != null)
         {
-            Instantiate(item.prefab, new Vector3(1000, 1000, 1003), Quaternion.identity);
+            itemPrefab = Instantiate(item.prefab, new Vector3(1000, 1000, 1003), Quaternion.identity);
+            //InspectInputController.Instance.OnEnable();
+            Debug.Log("Agnes: OnEquip in run");
+            playerInput.SwitchCurrentActionMap("Inspector");
+
         }
         else
         {
             Debug.LogWarning("Cannot spawn: Missing component, data, or prefab.");
         }
     }
+
+    // ACTION MAPS / INPUT HANDLING
 
     public void StopInspection()
     {
@@ -43,9 +63,51 @@ public class Inspector : MonoBehaviour
         // }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEnable()
     {
+        Debug.Log("Agnes: OnEquip in run");
+        playerInput.SwitchCurrentActionMap("Inspector");
+    }
+    public void OnDisable()
+    {
+        Debug.Log("Agnes: OnBack is run");
+        playerInput.SwitchCurrentActionMap("Player");
+    }
 
+    public void OnRotate(InputAction.CallbackContext context)
+    {
+        Debug.Log("Agnes: OnRotate is run");
+
+        if (context.started || context.performed)
+        {
+            isRotating = true;
+        }
+
+        if (context.canceled)
+        {
+            isRotating = false;
+        }
+    }
+
+
+    public void OnDelta(InputValue cc)
+    {
+        if (!isRotating || itemPrefab == null) return;
+
+        Vector2 delta = cc.Get<Vector2>();
+
+        itemPrefab.transform.Rotate(Vector3.up, -delta.x * 0.1f, Space.World);
+        itemPrefab.transform.Rotate(Vector3.right, delta.y * 0.1f, Space.World);
+    }
+
+
+
+    private void OnBack()
+    {
+        OnDisable();
+    }
+    private void OnEquip()
+    {
+        OnEnable();
     }
 }
